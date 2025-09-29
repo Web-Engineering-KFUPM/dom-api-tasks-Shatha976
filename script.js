7eb9eea1958d370ea345257d92b92a5eefcb1223
Shatha976 <s202283660@kfupm.edu.sa>
2025-09-29 11:49:31 +0300

    TODO1

--- script.js
+++ script.js
@@ -19,8 +19,7 @@ inside the <p> element with id="t1-msg".
 💡 Hint:
 document.getElementById("t1-msg").innerHTML = "Hello, World!";
 */
- 
-
+document.getElementById("t1-msg").innerHTML = "Hello, World!";
 /*  
 =======================================
 TODO2: Interaction Corner
